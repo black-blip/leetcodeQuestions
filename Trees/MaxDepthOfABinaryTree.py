6051800a26dde2e97d2943e4038dc9af98150b0f
Sakshi <65134782+black-blip@users.noreply.github.com>
2023-10-07 19:45:33 -0400

    Update MaxDepthOfABinaryTree.py

--- Trees/MaxDepthOfABinaryTree.py
+++ Trees/MaxDepthOfABinaryTree.py
@@ -4,10 +4,20 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+#use iterative DFS in this problem with stack and keep a record of the depth of each node 
 class Solution(object):
     def maxDepth(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-        
+        st = [(root,1),]
+        res = 0
+        while st:
+            node,depth = st.pop()
+            if node:
+                res = max(res,depth)
+                st.append((node.left,depth+1))
+                st.append((node.right,depth+1))
+        return res
