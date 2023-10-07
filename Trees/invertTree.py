34a537d6286a0fe4e26e304d2c66afc7a266cbdb
Sakshi <65134782+black-blip@users.noreply.github.com>
2023-10-07 19:36:23 -0400

    Update invertTree.py

--- Trees/invertTree.py
+++ Trees/invertTree.py
@@ -16,3 +16,28 @@ class Solution:
                 que.append(node.right)
         
         return root
+###DFS
+    def invertTreeDFS(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        if not root:
+            return None
+        
+
+        st = [root]
+        
+        while st:
+            node = st.pop()
+            if node:
+                node.left, node.right = node.right,node.left
+                st.extend([node.right,node.left])
+       
+        return root
+
+##Recursive
+    def invertTree(self,root: Optional[TreeNode])->Optional[TreeNode]:
+        if not root:
+            return None
+        root.left, root.right = root.right,root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
